5d89bf6b3ac2131dedd5413670b67a6fa042a022
Everton Christmann <evertonluiz00@hotmail.com>
2020-04-24 23:57:32 -0300

    Challenge done

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -6,6 +6,17 @@ interface Balance {
   total: number;
 }
 
+interface CreateTransaction {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
+interface Resume {
+  transactions: Transaction[];
+  balance: Balance;
+}
+
 class TransactionsRepository {
   private transactions: Transaction[];
 
@@ -14,15 +25,49 @@ class TransactionsRepository {
   }
 
   public all(): Transaction[] {
-    // TODO
+    return this.transactions;
   }
 
   public getBalance(): Balance {
-    // TODO
+    let totalIncomes = 0;
+    let totalOutcomes = 0;
+
+    const totalTransactions = this.transactions.reduce((total, transaction) => {
+      if (transaction.type === 'income') {
+        totalIncomes += transaction.value;
+      } else {
+        totalOutcomes += transaction.value;
+      }
+      return totalIncomes - totalOutcomes;
+    }, 0);
+
+    const balance = {
+      income: totalIncomes,
+      outcome: totalOutcomes,
+      total: totalTransactions,
+    };
+
+    return balance;
   }
 
-  public create(): Transaction {
-    // TODO
+  public getResume(): Resume {
+    const transactionsResume = this.all();
+    const balanceResume = this.getBalance();
+
+    const resume = {
+      transactions: transactionsResume,
+      balance: balanceResume,
+    };
+
+    return resume;
+  }
+
+  public create({ title, value, type }: CreateTransaction): Transaction {
+    const transaction = new Transaction({ title, value, type });
+
+    this.transactions.push(transaction);
+
+    return transaction;
   }
 }
 
